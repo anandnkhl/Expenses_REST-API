064dbc571e4e8c435a9986f76ce95dd7fd6da46b
nikhil <anandnikhil91@gmail.com>
2019-06-25 14:53:28 +0530

    Create & Delete working

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,20 +1,52 @@
 package handlers
 
 import (
+	"Expenses_REST-API/expenseDB"
 	"Expenses_REST-API/types"
+	"context"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
 	"strconv"
 	"time"
 )
-
 var expense types.Expense
 var expenses types.Expenses
+
+func CreateExpense(writer http.ResponseWriter, request *http.Request) {
+	data := &CreateExpenseRequest{}
+	err := render.Bind(request, data)
+	if err != nil {
+		_ = render.Render(writer, request, ErrInvalidRequest(err))
+		return
+	}
+
+	expense = *data.Expense
+	expense.Id = len(expenses) + 1
+	expense.CreatedOn = time.Now().String()
+	expense.UpdatedOn = time.Now().String()
+	expenses = append(expenses, expense)
+
+	ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
+	_, _ = expenseDB.ExpCollFunc().InsertOne(ctx, expense)
+	_ = render.Render(writer, request, NewExpenseResponse(&expense))
+}
+
+func DeleteExpense(writer http.ResponseWriter, request *http.Request) {
+
+	ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
+
+	filter := bson.D{{"id", ID}}
+	ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
+	expenseDB.ExpCollFunc().FindOneAndDelete(ctx, filter)
+}
+
 
 func UpdateExpense(writer http.ResponseWriter, request *http.Request) {
 	var data CreateExpenseRequest
 	ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
+
 	for index,exp := range expenses{
 		if exp.Id == ID{
 			idTemp := expenses[index].Id
@@ -23,20 +55,9 @@ func UpdateExpense(writer http.ResponseWriter, request *http.Request) {
 			_ = render.Bind(request, &data)
 
 			expenses[index] = *data.Expense
-			expenses[index].UpdatedOn = time.Now()
+			expenses[index].UpdatedOn = time.Now().String()
 			expenses[index].CreatedOn = createdOnTemp
 			expenses[index].Id = idTemp
-			return
-		}
-	}
-}
-
-func DeleteExpense(writer http.ResponseWriter, request *http.Request) {
-
-	ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
-	for index,exp := range expenses{
-		if exp.Id == ID{
-			expenses = append(expenses[:index], expenses[index + 1 :]... )
 			return
 		}
 	}
@@ -70,23 +91,6 @@ func AllExpensesResponse(exp *types.Expenses) *ExpensesResponse {
 	return resp
 }
 
-
-func CreateExpense(writer http.ResponseWriter, request *http.Request) {
-	data := &CreateExpenseRequest{}
-	err := render.Bind(request, data)
-	if err != nil {
-		_ = render.Render(writer, request, ErrInvalidRequest(err))
-		return
-	}
-
-	expense = *data.Expense
-	expense.Id = len(expenses) + 1
-	expense.CreatedOn = time.Now()
-	expense.UpdatedOn = time.Now()
-	expenses = append(expenses, expense)
-	_ = render.Render(writer, request, NewExpenseResponse(&expense))
-
-}
 
 type ExpenseResponse struct{
 	*types.Expense
