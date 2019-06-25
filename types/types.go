687a4dcd32eaca189c1e73069fe9d6efa82ee96a
nikhil <anandnikhil91@gmail.com>
2019-06-25 12:44:23 +0530

    Added bson in types

--- types/types.go
+++ types/types.go
@@ -1,16 +1,12 @@
 package types
 
-import (
-	"time"
-)
-
 type Expense struct{
-	Id 				int 		`json:"id"`
-	Description 	string 		`json:"description"`
-	Type 			string 		`json:"type"`
-	Amount 			float64 	`json:"amount"`
-	CreatedOn 		time.Time 	`json:"created_on"`
-	UpdatedOn 		time.Time 	`json:"updated_on"`
+	Id 				int 		`json:"id" bson:"id"`
+	Description 	string 		`json:"description" bson:"description"`
+	Type 			string 		`json:"type" json:"type"`
+	Amount 			float64 	`json:"amount" bson:"amount"`
+	CreatedOn 		string 	`json:"created_on" bson:"created_on"`
+	UpdatedOn 		string 	`json:"updated_on" bson:"updated_on"`
 }
 
 type Expenses []Expense
