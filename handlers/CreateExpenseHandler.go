d8629b7cd3386d34d1c778b029f2b65ca9086119
nikhil <anandnikhil91@gmail.com>
2019-06-26 12:02:46 +0530

    Create Handler refactored

---
+++ handlers/CreateExpenseHandler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"context"
+	"github.com/go-chi/render"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func (mongo *MongoDB)CreateExpense(writer http.ResponseWriter, request *http.Request) {
+
+	timeIDStr := time.Now().String()
+	timeIDStr = timeIDStr[:4]+timeIDStr[5:7]+timeIDStr[8:10]+timeIDStr[11:13]+ timeIDStr[14:16]+timeIDStr[17:19]+timeIDStr[21:23]
+	timeIDInt,_ := strconv.Atoi(timeIDStr)
+
+	data := &CreateExpenseRequest{}
+	err := render.Bind(request, data)
+	if err != nil {
+		_ = render.Render(writer, request, ErrInvalidRequest(err))
+		return
+	}
+
+	expense := *data.Expense
+	expense.Id = timeIDInt
+	expense.CreatedOn = time.Now().String()
+	expense.UpdatedOn = time.Now().String()
+
+	ctx,_ := context.WithTimeout(context.Background(), 15*time.Second)
+	_, _ = mongo.Db.InsertOne(ctx, expense)
+
+	_ = render.Render(writer, request, NewExpenseResponse(&expense))
+}
