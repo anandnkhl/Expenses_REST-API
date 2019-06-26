644d1344c3a6214fe47b0f3acf37d4d7f01f48a0
nikhil <anandnikhil91@gmail.com>
2019-06-26 12:18:13 +0530

    ListOne Handler Refactored

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,32 +2,13 @@ package handlers
 
 import (
 	"Expenses_REST-API/types"
-	"context"
-	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
-	"strconv"
-	"time"
 )
 type MongoDB struct{
 	Db *mongo.Collection
 }
-
-
-
-
-func (mongo *MongoDB)GetId(writer http.ResponseWriter, request *http.Request) {
-	ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
-	ctx,_ := context.WithTimeout(context.Background(), 10*time.Second)
-	curr:= mongo.Db.FindOne(ctx, bson.D{{"id", ID}})
-	var expense types.Expense
-	_ = curr.Decode(&expense)
-	_=render.Render(writer, request, NewExpenseResponse(&expense))
-}
-
-
 
 type ExpensesResponse struct {
 	*types.Expenses
