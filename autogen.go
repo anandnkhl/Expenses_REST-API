062ae0ef37e175f84a8999c986e0a9ecbcce03ca
nikhil <anandnikhil91@gmail.com>
2019-07-05 18:21:32 +0530

    autogen working

--- autogen.go
+++ autogen.go
@@ -1,9 +1,36 @@
 package main
 
-type MetaData struct{
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+type MetaData struct{
+	DbObj string
+	DbType string
+	Operation string
 }
 
 func main(){
+	metadata := MetaData{
+		DbObj: "mongo",
+		DbType: "MongoDB",
+		Operation: "default",
+	}
+	file,err := os.Create("./handlers/"+metadata.Operation+"Handler.go")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	templFile, err := ioutil.ReadFile("./templates/HandlerTempl.gotpl")
+	tpl,err := template.New("template").Parse(string(templFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tpl.Execute(file, metadata)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
