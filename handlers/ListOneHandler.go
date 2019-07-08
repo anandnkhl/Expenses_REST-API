54b02d64dd4e1cc4ae128728c2e5d292daf9ccc8
nikhil <anandnikhil91@gmail.com>
2019-07-08 15:49:42 +0530

    DB added in ListAll

--- handlers/ListOneHandler.go
+++ handlers/ListOneHandler.go
@@ -1,14 +1,22 @@
 package handlers
 
 import (
+    "Expenses_REST-API/types"
     "context"
+    "github.com/go-chi/chi"
+    "github.com/go-chi/render"
+    "go.mongodb.org/mongo-driver/bson"
     "net/http"
+    "strconv"
     "time"
 )
 
-func ( m *MongoDB ) ListOneExpense (writer http.ResponseWriter, request *http.Request){
+func ( m *MongoDB ) GetId (writer http.ResponseWriter, request *http.Request){
+    ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
     ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
 
+    curr:= m.Db.FindOne(ctx, bson.D{{"id", ID}})
+    var expense types.Expense
+    _ = curr.Decode(&expense)
     //TODO implement the renderer
-    //_ = render.Render(writer, request, RendererFunc(&obj))
-}
+    _=render.Render(writer, request, NewExpenseResponse(&expense))}
