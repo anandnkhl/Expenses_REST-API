4d432d6e6582e6c78972efcc54a5c9f0d7562257
nikhil <anandnikhil91@gmail.com>
2019-07-08 15:19:23 +0530

    DB added in generated Create

--- handlers/CreateHandler.go
+++ handlers/CreateHandler.go
@@ -2,13 +2,32 @@ package handlers
 
 import (
     "context"
+    "github.com/go-chi/render"
     "net/http"
+    "strconv"
     "time"
 )
 
 func ( m *MongoDB ) CreateExpense (writer http.ResponseWriter, request *http.Request){
-    ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
 
+    timeIDStr := time.Now().String()
+    timeIDStr = timeIDStr[:4]+timeIDStr[5:7]+timeIDStr[8:10]+timeIDStr[11:13]+ timeIDStr[14:16]+timeIDStr[17:19]+timeIDStr[21:23]
+    timeIDInt,_ := strconv.Atoi(timeIDStr)
+
+    data := &CreateExpenseRequest{}
+    err := render.Bind(request, data)
+    if err != nil {
+        _ = render.Render(writer, request, ErrInvalidRequest(err))
+        return
+    }
+
+    expense := *data.Expense
+    expense.Id = timeIDInt
+    expense.CreatedOn = time.Now().String()
+    expense.UpdatedOn = time.Now().String()
+
+    ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
+    _, _ = m.Db.InsertOne(ctx, expense)
     //TODO implement the renderer
-    //_ = render.Render(writer, request, RendererFunc(&obj))
+    _ = render.Render(writer, request, NewExpenseResponse(&expense))
 }
