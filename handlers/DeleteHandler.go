6fbcacb35daa1a48788b6937cb98dbd6d57bc393
nikhil <anandnikhil91@gmail.com>
2019-07-08 15:29:33 +0530

    Added DB in generated Delete

--- handlers/DeleteHandler.go
+++ handlers/DeleteHandler.go
@@ -2,12 +2,19 @@ package handlers
 
 import (
     "context"
+    "github.com/go-chi/chi"
+    "go.mongodb.org/mongo-driver/bson"
     "net/http"
+    "strconv"
     "time"
 )
 
 func ( m *MongoDB ) DeleteExpense (writer http.ResponseWriter, request *http.Request){
+    ID, _ := strconv.Atoi(chi.URLParam(request, "ID"))
+    filter := bson.D{{"id", ID}}
     ctx,_ := context.WithTimeout(context.Background(), 5*time.Second)
+
+    m.Db.FindOneAndDelete(ctx, filter)
 
     //TODO implement the renderer
     //_ = render.Render(writer, request, RendererFunc(&obj))
